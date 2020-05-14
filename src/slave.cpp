71640b54e831b2897a066cc95afa19953327910d
robinuyttendaele <57678877+robinuyttendaele@users.noreply.github.com>
2020-05-14 20:28:06 +0200

    Finalize test A4988

--- src/slave.cpp
+++ src/slave.cpp
@@ -1,23 +1,48 @@
 #ifndef MASTER
 #include <Arduino.h>
-#define MS1Pin 2
-#define MS2Pin 3
-#define MS3Pin 4
-#define stepPin 5
-#define dirPin 6
+#define stepPin 2
+#define dirPin 3
+float angle = 0;
+float dif = 0;
+float remainder = 0;
+int length = 0;
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(MS1Pin, OUTPUT);
-  pinMode(MS2Pin, OUTPUT);
-  pinMode(MS3Pin, OUTPUT);
-  pinMode(stepPin, OUTPUT);
-  pinMode(dirPin, OUTPUT);
+  
 
   
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  //Only do something if a new serial is available, coming from the bluetooth module
+  if(Serial.available()>0){
+    //dif = Serial.read();
+
+    //Set the right direction in which the motor will rotate (depends of the connection of the coils)
+    if(dif<0){
+      digitalWrite(dirPin, LOW);
+      //Update to new angle which is a multiple of 22.5, this is needed for the functionality with the switches
+      remainder = fmod(abs(dif), 22.5);
+      if(remainder>11.25) dif += remainder - 22.5;
+      else dif += remainder;
+    }
+    else {
+      digitalWrite(dirPin, HIGH);
+      //Update to new angle which is a multiple of 22.5
+      remainder = fmod(dif, 22.5);
+      if(remainder>11.25) dif += 22.5 - remainder;
+      else dif -= remainder;     
+    }
+    
+    //Make the clock pulse with the right length
+    length = 2*dif/0.9;
+    //Move the motor
+    for (int i = 0; i < length; i++){
+      digitalWrite(stepPin, !digitalRead(stepPin));
+      delayMicroseconds(500);
+    }
+  }
 }
 #endif
--- test/A4988/A4988.cpp
+++ test/A4988/A4988.cpp
@@ -1,10 +1,6 @@
 #include <Arduino.h>
 #define stepPin 2 //D2
 #define dirPin 3  //D3
-float angle = 0;
-float dif = 0;
-float remainder = 0;
-int length = 0;
 
 void setup() {
   // put your setup code here, to run once:
@@ -15,35 +11,10 @@ void setup() {
 
   digitalWrite(dirPin, LOW);
   digitalWrite(stepPin, LOW);
-}
-
-void loop() {
-  //Only do something if a new serial is available, coming from the bluetooth module
-  if(Serial.availableForWrite()){
-    dif = Serial.read();
-
-    //Set the right direction in which the motor will rotate (depends of the connection of the coils)
-    if(dif<0){
-      digitalWrite(dirPin, LOW);
-      //Update to new angle which is a multiple of 22.5, this is needed for the functionality with the switches
-      remainder = fmod(abs(dif), 22.5);
-      if(remainder>11.25) dif += remainder - 22.5;
-      else dif += remainder;
-    }
-    else {
-      digitalWrite(dirPin, HIGH);
-      //Update to new angle which is a multiple of 22.5
-      remainder = fmod(dif, 22.5);
-      if(remainder>11.25) dif += 22.5 - remainder;
-      else dif -= remainder;     
-    }
-    
-    //Make the clock pulse with the right length
-    length = 2*dif/0.9;
-    //Move the motor
-    for (int i = 0; i < length; i++){
+  for (int i = 0; i < 800; i++){
       digitalWrite(stepPin, !digitalRead(stepPin));
       delayMicroseconds(500);
-    }
-  }
+ }
 }
+
+void loop() {}
